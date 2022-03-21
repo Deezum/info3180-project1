1d3e84c9d4810d2daded8782e72223224f477f26
Deezum <beadleselena@gmail.com>
2022-03-20 21:56:52 -0500

    edit1

--- app/config.py
+++ app/config.py
@@ -8,6 +8,6 @@ class Config(object):
     UPLOAD_FOLDER = './uploads'
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace ('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('DATABASE_URL') 
 
